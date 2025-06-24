0a97ef7e8216b4d2302fd72d573f2ac4123d1dfe
memmelma <marius.memmel@gmx.net>
2025-06-23 17:00:18 -0700

    refactor

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robotic",
+    version="0.1.0",
+    description="Robotic learning project with 3D diffusion models",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+)
